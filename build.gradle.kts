f4d77a309c99f25da710d2444100aaf7e4030bdc
h-beeen <happybean@hectodata.co.kr>
2025-01-15 13:36:55 +0900

    chore: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+group = "dev"
+version = "0.0.1-SNAPSHOT"
+
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+dependencies {
+	// Spring & Webflux
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+	// Kotlin
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	// Coroutines & Reactor
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
+	// Test
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(module = "mockito-core")
+	}
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+repositories {
+	mavenCentral()
+}
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(21))
+	}
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
